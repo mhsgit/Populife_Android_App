29d40d4591469bb4ce7112dcb01937008b5e4b6b
mhsgit <1411826764@qq.com>
2025-08-11 22:28:40 +0800

    Revert "登录和注册页面-中文场景-去除手机验证码校验"
    This reverts commit 4eba9fffbd21a2b4541630f119d46dfd64c201d2.

--- app/src/main/java/com/populstay/populife/activity/SignActivity.java
+++ app/src/main/java/com/populstay/populife/activity/SignActivity.java
@@ -236,7 +236,7 @@ public class SignActivity extends BaseActivity implements View.OnClickListener, 
 
 		if (isChineseLanguage()) {
 			if (VAL_ACCOUNT_SIGN_IN.equals(mAccountActionType)) {
-				mTvSwitchSignType.setVisibility(View.GONE);
+				mTvSwitchSignType.setVisibility(View.VISIBLE);
 				mTvSwitchSignType.setText(getResources().getString(R.string.sign_in_by_verify_code));
 				mEtUserName.setType(ExEditText.TYPE_ACCOUNT);
 				mEtUserName.setLabel("");
@@ -338,11 +338,7 @@ public class SignActivity extends BaseActivity implements View.OnClickListener, 
 				mTvSwitchSignType.setVisibility(View.GONE);
 				mRlUserTerms.setVisibility(View.VISIBLE);
 				mEtConfirmPwd.setVisibility(View.VISIBLE);
-				if (isChineseLanguage()){
-					mEtCode.setVisibility(View.GONE);
-				}else {
-					mEtCode.setVisibility(View.VISIBLE);
-				}
+				mEtCode.setVisibility(View.VISIBLE);
 				ll_switch_language.setVisibility(View.VISIBLE);
 				break;
 
@@ -548,11 +544,7 @@ public class SignActivity extends BaseActivity implements View.OnClickListener, 
 				isEnable = isNotEmptyUserName && isNotEmptyCode;
 				break;
 			case VAL_ACCOUNT_SIGN_UP:
-				if (isChineseLanguage()){
-					isEnable = isNotEmptyUserName && isNotEmptyPwd && isNotEmptyConfirmPwd && isSelectedUserTerms;
-				}else {
-					isEnable = isNotEmptyUserName && isNotEmptyPwd && isNotEmptyConfirmPwd && isNotEmptyCode && isSelectedUserTerms;
-				}
+				isEnable = isNotEmptyUserName && isNotEmptyPwd && isNotEmptyConfirmPwd && isNotEmptyCode && isSelectedUserTerms;
 				break;
 			case VAL_ACCOUNT_RESET_PWD:
 				isEnable = isNotEmptyPwd && isNotEmptyConfirmPwd;
